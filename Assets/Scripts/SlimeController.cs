8da51afc1ce2f06f9ffc256c81d136ac727914a7
Eric Sexton <foopis23@gmail.com>
2021-02-18 00:10:41 -0500

    got some stuff working

--- Assets/Scripts/SlimeController.cs
+++ Assets/Scripts/SlimeController.cs
@@ -8,6 +8,8 @@ public class SlimeController : MonoBehaviour
     //editor properties
     [SerializeField] private float dragDeadZone = 10.0f;
     [SerializeField] private LayerMask selectionLayer;
+    [SerializeField] private Tilemap overlay;
+    [SerializeField] private TileBase possibleMoveTile;
     [SerializeField] private Tilemap tilemap;
     [SerializeField] private GameObject slimePrefab;
     [SerializeField] private GameObject tileSelectionCursor;
@@ -32,11 +34,12 @@ public class SlimeController : MonoBehaviour
         mergeWith = null;
         isValidTile = false;
 
-        if (currentSlime == null) 
+        if (currentSlime == null)
         {
             currentSlimeObject = GameObject.FindGameObjectWithTag("slime");
             currentSlime = currentSlimeObject?.GetComponent<Slime>();
             currentSlimeTileLocation = tilemap.WorldToCell(new Vector3(currentSlimeObject.transform.position.x, currentSlimeObject.transform.position.y, 0));
+            ShowPossibleMoves();
         }
     }
 
@@ -55,22 +58,25 @@ public class SlimeController : MonoBehaviour
         }
     }
 
-    private void AddSlime(GameObject slime) {
+    private void AddSlime(GameObject slime)
+    {
         Vector3Int gridPos = tilemap.WorldToCell(new Vector3(slime.transform.position.x, slime.transform.position.y, 0));
         Debug.Log($"Add: {gridPos}");
         slimes.Add(gridPos, slime);
     }
 
-    private GameObject GetSlime(Vector3 worldPos) {
-        Vector3Int gridPos = tilemap.WorldToCell(new Vector3(worldPos.x,worldPos.y, 0));
+    private GameObject GetSlime(Vector3 worldPos)
+    {
+        Vector3Int gridPos = tilemap.WorldToCell(new Vector3(worldPos.x, worldPos.y, 0));
         Debug.Log($"Get: {gridPos}");
         if (!slimes.ContainsKey(gridPos)) return null;
 
         return slimes[gridPos];
     }
 
-    private bool RemoveSlime(Vector3 worldPos) {
-        Vector3Int gridPos = tilemap.WorldToCell(new Vector3(worldPos.x,worldPos.y, 0));
+    private bool RemoveSlime(Vector3 worldPos)
+    {
+        Vector3Int gridPos = tilemap.WorldToCell(new Vector3(worldPos.x, worldPos.y, 0));
         Debug.Log($"Add: {gridPos}");
         return slimes.Remove(gridPos);
     }
@@ -88,22 +94,171 @@ public class SlimeController : MonoBehaviour
         slime.transform.position = new Vector2(gridWorldPos.x, gridWorldPos.y);
     }
 
-    private void MoveSelectedSlime(Vector3 worldPos) {
-        GameObject temp = GetSlime(worldPos);
-        
-        if (temp != null) {
-            Slime slime = temp.GetComponent<Slime>();
+    private void ShowPossibleMoves()
+    {
+        overlay.ClearAllTiles();
+        int width = currentSlime.Scale + 1;
+
+        Debug.Log(width);
+
+        //neg x
+        Vector3Int testPos = new Vector3Int(currentSlimeTileLocation.x - 1, currentSlimeTileLocation.y, currentSlimeTileLocation.z);
+        while (tilemap.HasTile(testPos))
+        {
+            //check if every tile in the width is valid
+            for (int w=0; w < width; w++) {
+                Vector3Int widthTestPos = new Vector3Int(testPos.x, testPos.y+w, testPos.z);
+                if (!tilemap.HasTile(widthTestPos)) break;
+            }
+
+            //add a tile for each tile in the width
+            for (int w=0; w < width; w++) {
+                Vector3Int widthTestPos = new Vector3Int(testPos.x, testPos.y+w, testPos.z);
+                overlay.SetTile(widthTestPos, possibleMoveTile);
+            }
             
-            if (currentSlime.CanMerge() && slime.Scale == currentSlime.Scale) {
-                currentSlime.SetDestination(roundToGrid(worldPos));
+            testPos = new Vector3Int(testPos.x - 1, testPos.y, testPos.z);
+        }
+
+        //pos x
+        testPos = new Vector3Int(currentSlimeTileLocation.x + 1, currentSlimeTileLocation.y, currentSlimeTileLocation.z);
+        while (tilemap.HasTile(testPos))
+        {
+            //check if every tile in the width is valid
+            for (int w=0; w < width; w++) {
+                Vector3Int widthTestPos = new Vector3Int(testPos.x, testPos.y+w, testPos.z);
+                if (!tilemap.HasTile(widthTestPos)) break;
+            }
+
+            //add a tile for each tile in the width
+            for (int w=0; w < width; w++) {
+                Vector3Int widthTestPos = new Vector3Int(testPos.x, testPos.y+w, testPos.z);
+                overlay.SetTile(widthTestPos, possibleMoveTile);
+            }
+
+            testPos = new Vector3Int(testPos.x + 1, testPos.y, testPos.z);
+        }
+
+        //neg y
+        testPos = new Vector3Int(currentSlimeTileLocation.x, currentSlimeTileLocation.y - 1, currentSlimeTileLocation.z);
+        while (tilemap.HasTile(testPos))
+        {
+            //check if every tile in the width is valid
+            for (int w=0; w < width; w++) {
+                Vector3Int widthTestPos = new Vector3Int(testPos.x+w, testPos.y, testPos.z);
+                if (!tilemap.HasTile(widthTestPos)) break;
+            }
+
+            //add a tile for each tile in the width
+            for (int w=0; w < width; w++) {
+                Vector3Int widthTestPos = new Vector3Int(testPos.x+w, testPos.y, testPos.z);
+                overlay.SetTile(widthTestPos, possibleMoveTile);
+            }
+
+            testPos = new Vector3Int(testPos.x, testPos.y - 1, testPos.z);
+        }
+
+        //pos y
+        testPos = new Vector3Int(currentSlimeTileLocation.x, currentSlimeTileLocation.y + 1, currentSlimeTileLocation.z);
+        while (tilemap.HasTile(testPos))
+        {
+            //check if every tile in the width is valid
+            for (int w=0; w < width; w++) {
+                Vector3Int widthTestPos = new Vector3Int(testPos.x+w, testPos.y, testPos.z);
+                if (!tilemap.HasTile(widthTestPos)) break;
+            }
+
+            //add a tile for each tile in the width
+            for (int w=0; w < width; w++) {
+                Vector3Int widthTestPos = new Vector3Int(testPos.x+w, testPos.y, testPos.z);
+                overlay.SetTile(widthTestPos, possibleMoveTile);
+            }
+
+            testPos = new Vector3Int(testPos.x, testPos.y + 1, testPos.z);
+        }
+    }
+
+    private bool IsValidMove(Vector3Int mapTileLocation)
+    {
+        if ((mapTileLocation.x == currentSlimeTileLocation.x || mapTileLocation.y == currentSlimeTileLocation.y) && tilemap.HasTile(mapTileLocation))
+        {
+            if (currentSlimeTileLocation.x == mapTileLocation.x)
+            {
+                int start;
+                int end;
+
+                if (mapTileLocation.y > currentSlimeTileLocation.y)
+                {
+                    start = currentSlimeTileLocation.y;
+                    end = mapTileLocation.y;
+                }
+                else
+                {
+                    start = mapTileLocation.y;
+                    end = currentSlimeTileLocation.y;
+                }
+
+                for (int i = start; i <= end; i++)
+                {
+                    Vector3Int testPos = new Vector3Int(currentSlimeTileLocation.x, i, 0);
+                    if (!tilemap.HasTile(testPos)) return false;
+                }
+            }
+            else
+            {
+                int start;
+                int end;
+
+                if (mapTileLocation.x > currentSlimeTileLocation.x)
+                {
+                    start = currentSlimeTileLocation.x;
+                    end = mapTileLocation.x;
+                }
+                else
+                {
+                    start = mapTileLocation.x;
+                    end = currentSlimeTileLocation.x;
+                }
+
+                for (int i = start; i <= end; i++)
+                {
+                    Vector3Int testPos = new Vector3Int(i, currentSlimeTileLocation.y, 0);
+                    if (!tilemap.HasTile(testPos)) return false;
+                }
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void MoveSelectedSlime(Vector3 worldPos)
+    {
+        GameObject temp = GetSlime(worldPos);
+
+        if (temp != null)
+        {
+            Slime slime = temp.GetComponent<Slime>();
+
+            if (currentSlime.CanMerge() && slime.Scale == currentSlime.Scale)
+            {
+                Vector3 pos = roundToGrid(worldPos);
+                currentSlime.SetDestination(new Vector3(pos.x, pos.y, currentSlimeObject.transform.position.z));
                 currentSlimeTileLocation = tilemap.WorldToCell(new Vector3(worldPos.x, worldPos.y, 0));
+                ShowPossibleMoves();
                 mergeWith = temp;
                 RemoveSlime(worldPos);
             }
 
-        }else{
+        }
+        else
+        {
+            Vector3 pos = roundToGrid(worldPos);
+            currentSlime.SetDestination(new Vector3(pos.x, pos.y, currentSlimeObject.transform.position.z));
             currentSlimeTileLocation = tilemap.WorldToCell(new Vector3(worldPos.x, worldPos.y, 0));
-            currentSlime.SetDestination(roundToGrid(worldPos));
+            ShowPossibleMoves();
         }
     }
 
@@ -117,13 +272,16 @@ public class SlimeController : MonoBehaviour
         {
             if (isDragging)
             {
-                if (hasSpawnedSplitFromDrag && isValidTile) {
+                if (hasSpawnedSplitFromDrag && isValidTile)
+                {
                     MoveSelectedSlime(mouseWorldPos);
                 }
 
                 hasSpawnedSplitFromDrag = false;
 
-            }else if (currentSlimeObject != null && isValidTile) {
+            }
+            else if (currentSlimeObject != null && isValidTile)
+            {
                 MoveSelectedSlime(mouseWorldPos);
             }
 
@@ -156,19 +314,23 @@ public class SlimeController : MonoBehaviour
             }
         }
 
-        if (mergeWith != null && !currentSlime.IsMoving) {
+        if (mergeWith != null && !currentSlime.IsMoving)
+        {
             currentSlime.Merge();
             Destroy(mergeWith);
             mergeWith = null;
         }
 
         tileSelectionCursor.transform.position = roundToGrid(mouseWorldPos);
-        
-        isValidTile = (mapTileLocation.x == currentSlimeTileLocation.x || mapTileLocation.y == currentSlimeTileLocation.y) && tilemap.HasTile(mapTileLocation);
-        if (isValidTile) {
-            tileSelectionCursor.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0.5f);
-        }else{
-           tileSelectionCursor.GetComponent<SpriteRenderer>().color = new Color(255, 0, 0, 0.5f); 
+        isValidTile = IsValidMove(mapTileLocation);
+
+        if (isValidTile)
+        {
+            tileSelectionCursor.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0.5f);
+        }
+        else
+        {
+            tileSelectionCursor.GetComponent<SpriteRenderer>().color = new Color(255, 0, 0, 0.5f);
         }
     }
 }
