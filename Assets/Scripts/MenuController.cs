c7e600b3560f5ab66e0fcee9566b34e6c133ee0b
Eric Sexton <foopis23@gmail.com>
2021-02-18 18:46:00 -0500

    Setup Basic Menu Animation

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject MainMenu;
+    [SerializeField] private GameObject MainMenuUI;
+    private Animator MainMenuAnimator;
+    [SerializeField] private GameObject LevelSelect;
+    [SerializeField] private GameObject LevelSelectUI;
+    private Animator LevelSelectAnimator;
+    [SerializeField] private GameObject Options;
+    private Animator OptionsAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenuAnimator = MainMenu.GetComponent<Animator>();
+        LevelSelectAnimator = LevelSelect.GetComponent<Animator>();
+        // OptionsAnimator = LevelSelect.GetComponent<Animator>();
+    }
+
+    public void OnPlayClicked()
+    {
+        MainMenuAnimator.Play("SlideLeftOut");
+        LevelSelectAnimator.Play("SlideLeftIn");
+        LevelSelectUI.SetActive(true);
+        //MainMenuUI.SetActive(false);
+    }
+
+    void OnOptionsClicked()
+    {
+
+    }
+
+    void OnOptionsBackClicked()
+    {
+
+    }
+
+    void OnLevelSelectBackClicked()
+    {
+
+    }
+}
